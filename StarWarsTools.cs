e4fb20915bd0c2964b75388c3374dc267d57c953
Jake Kirkbride <jakelegendxiii@gmail.com>
2026-01-14 14:52:37 -0500

    Add project files.

---
+++ StarWarsTools.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+using System.Text.Json;
+
+using ModelContextProtocol.Server;
+
+namespace StarWarsMCPServer;
+
+[McpServerToolType]
+public static class StarWarsTools
+{
+}
